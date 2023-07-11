47f132034d53dffacbefbfc16c44680f014613a5
kobakon <138418858+kobakon@users.noreply.github.com>
2023-07-11 14:56:29 +0000

    [Finish]投稿機能・一覧機能・詳細機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,33 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  #データを追加、保存する
+  def create
+    #データを受け取り新規登録するためのインスタンス作成
+    book =Book.new(book_params)
+
+    #データをデータベースに保存するためのsaveメソッド実行
+    book.save
+
+    #フラッシュメッセージ実装
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
